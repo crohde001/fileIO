7c16dd3531bd15542f480ce9e11bb21e8febf40d
Christopher Rohde <mail@chrisrohde.com>
2020-01-12 18:11:45 -0500

    Add a for each loop and exported data to text file.

--- fileIO.py
+++ fileIO.py
@@ -1,6 +1,15 @@
-f = open('inputFile.txt')
-count=0
+
+
+f = open('inputFile.txt', 'r')
+passfile = open('passfile.txt', 'w')
+failfile = open('failfile.txt', 'w' )
 for line in f:
-    print(str(count) + " " + line)
-    count = count + 2
+    line_split = line.split()
+    if line_split[2] =='P':
+        passfile.write(line)
+    else:
+        failfile.write(line)    
 f.close()
+passfile.close()
+failfile.close()
+
